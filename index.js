c3df47853a1e93976ddf162e5a9e416ad6059028
Dan Brown <dan@stompydan.net>
2017-04-17 20:45:48 +0100

    CoffeeScript -> JS + Pretter

---
+++ index.js
@@ -0,0 +1,469 @@
+'use babel';
+
+import path from 'path';
+import TermView from './lib/term-view';
+import ListView from './lib/build/list-view';
+import store from './lib/store';
+import { Emitter } from 'event-kit';
+const keypather = require('keypather')();
+import { CompositeDisposable } from 'event-kit';
+
+const capitalize = str => str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+const getColors = function() {
+  const {
+    normalBlack,
+    normalRed,
+    normalGreen,
+    normalYellow,
+    normalBlue,
+    normalPurple,
+    normalCyan,
+    normalWhite,
+    brightBlack,
+    brightRed,
+    brightGreen,
+    brightYellow,
+    brightBlue,
+    brightPurple,
+    brightCyan,
+    brightWhite,
+    background,
+    foreground,
+  } = atom.config.getAll('term3.colors')[0].value;
+  return [
+    normalBlack,
+    normalRed,
+    normalGreen,
+    normalYellow,
+    normalBlue,
+    normalPurple,
+    normalCyan,
+    normalWhite,
+    brightBlack,
+    brightRed,
+    brightGreen,
+    brightYellow,
+    brightBlue,
+    brightPurple,
+    brightCyan,
+    brightWhite,
+    background,
+    foreground,
+  ].map(color => color.toHexString());
+};
+
+const config = {
+  autoRunCommand: {
+    type: 'string',
+    default: '',
+  },
+  titleTemplate: {
+    type: 'string',
+    default: 'Terminal ({{ bashName }})',
+  },
+  fontFamily: {
+    type: 'string',
+    default: '',
+  },
+  fontSize: {
+    type: 'string',
+    default: '',
+  },
+  colors: {
+    type: 'object',
+    properties: {
+      normalBlack: {
+        type: 'color',
+        default: '#2e3436',
+      },
+      normalRed: {
+        type: 'color',
+        default: '#cc0000',
+      },
+      normalGreen: {
+        type: 'color',
+        default: '#4e9a06',
+      },
+      normalYellow: {
+        type: 'color',
+        default: '#c4a000',
+      },
+      normalBlue: {
+        type: 'color',
+        default: '#3465a4',
+      },
+      normalPurple: {
+        type: 'color',
+        default: '#75507b',
+      },
+      normalCyan: {
+        type: 'color',
+        default: '#06989a',
+      },
+      normalWhite: {
+        type: 'color',
+        default: '#d3d7cf',
+      },
+      brightBlack: {
+        type: 'color',
+        default: '#555753',
+      },
+      brightRed: {
+        type: 'color',
+        default: '#ef2929',
+      },
+      brightGreen: {
+        type: 'color',
+        default: '#8ae234',
+      },
+      brightYellow: {
+        type: 'color',
+        default: '#fce94f',
+      },
+      brightBlue: {
+        type: 'color',
+        default: '#729fcf',
+      },
+      brightPurple: {
+        type: 'color',
+        default: '#ad7fa8',
+      },
+      brightCyan: {
+        type: 'color',
+        default: '#34e2e2',
+      },
+      brightWhite: {
+        type: 'color',
+        default: '#eeeeec',
+      },
+      background: {
+        type: 'color',
+        default: '#000000',
+      },
+      foreground: {
+        type: 'color',
+        default: '#f0f0f0',
+      },
+    },
+  },
+  scrollback: {
+    type: 'integer',
+    default: 1000,
+  },
+  cursorBlink: {
+    type: 'boolean',
+    default: true,
+  },
+  shellOverride: {
+    type: 'string',
+    default: '',
+  },
+  shellArguments: {
+    type: 'string',
+    default: (function({ SHELL, HOME }) {
+      switch (path.basename(SHELL && SHELL.toLowerCase())) {
+        case 'bash':
+          return `--init-file ${path.join(HOME, '.bash_profile')}`;
+        case 'zsh':
+          return '-l';
+        default:
+          return '';
+      }
+    })(process.env),
+  },
+  openPanesInSameSplit: {
+    type: 'boolean',
+    default: false,
+  },
+};
+
+export default {
+  termViews: [],
+  focusedTerminal: false,
+  emitter: new Emitter(),
+  config,
+  disposables: null,
+
+  activate(state) {
+    this.state = state;
+    this.disposables = new CompositeDisposable();
+
+    if (!process.env.LANG) {
+      console.warn(
+        'Term3: LANG environment variable is not set. Fancy characters (å, ñ, ó, etc`) may be corrupted. The only work-around is to quit Atom and run `atom` from your shell.',
+      );
+    }
+
+    ['up', 'right', 'down', 'left'].forEach(direction => {
+      return this.disposables.add(
+        atom.commands.add(
+          'atom-workspace',
+          `term3:open-split-${direction}`,
+          this.splitTerm.bind(this, direction),
+        ),
+      );
+    });
+
+    this.disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'term3:open',
+        this.newTerm.bind(this),
+      ),
+    );
+    this.disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'term3:pipe-path',
+        this.pipeTerm.bind(this, 'path'),
+      ),
+    );
+    this.disposables.add(
+      atom.commands.add(
+        'atom-workspace',
+        'term3:pipe-selection',
+        this.pipeTerm.bind(this, 'selection'),
+      ),
+    );
+
+    return this.disposables.add(
+      atom.packages.onDidActivatePackage(function(pkg) {
+        if (pkg.name !== 'tree-view') {
+          return;
+        }
+        const node = new ListView();
+        const treeView = pkg.mainModule.treeView.element;
+        const el = treeView.querySelector('.tree-view-scroller');
+        return el.insertBefore(node, el.firstChild);
+      }),
+    );
+  },
+
+  service_0_1_3() {
+    return {
+      getTerminals: this.getTerminals.bind(this),
+      onTerm: this.onTerm.bind(this),
+      newTerm: this.newTerm.bind(this),
+    };
+  },
+
+  getTerminals() {
+    return store.getState().terminals.map(t => t.term);
+  },
+
+  onTerm(callback) {
+    return this.emitter.on('term', callback);
+  },
+
+  attachSubscriptions(termView, item, pane) {
+    const subscriptions = new CompositeDisposable();
+
+    const focusNextTick = () => process.nextTick(() => termView.focus());
+
+    subscriptions.add(
+      pane.onDidActivate(function() {
+        const activeItem = pane.getActiveItem();
+        if (activeItem !== item) {
+          return;
+        }
+        termView.focus();
+        return focusNextTick(activeItem);
+      }),
+    );
+
+    subscriptions.add(
+      pane.onDidChangeActiveItem(function(activeItem) {
+        if (activeItem !== termView) {
+          if (termView.term) {
+            termView.term.constructor._textarea = null;
+          }
+          return;
+        }
+        return focusNextTick(activeItem);
+      }),
+    );
+
+    subscriptions.add(
+      termView.onExit(function() {
+        if (this.focusedTerminal === termView) {
+          this.focusedTerminal = false;
+        }
+        return store.removeTerminal(termView);
+      }),
+    );
+
+    subscriptions.add(
+      termView.onFocus(() => {
+        return (this.focusedTerminal = termView);
+      }),
+    );
+
+    subscriptions.add(
+      pane.onWillRemoveItem(itemRemoved => {
+        if (itemRemoved.item === item) {
+          item.destroy();
+          store.removeTerminal(termView);
+          this.disposables.remove(subscriptions);
+          return subscriptions.dispose();
+        }
+      }),
+    );
+
+    return subscriptions;
+  },
+
+  newTerm(forkPTY = true, rows = 30, cols = 80, title = 'tty') {
+    const termView = this.createTermView(forkPTY, rows, cols, title);
+    const pane = atom.workspace.getActivePane();
+    const item = pane.addItem(termView);
+    this.disposables.add(this.attachSubscriptions(termView, item, pane));
+    pane.activateItem(item);
+    return termView;
+  },
+
+  createTermView(forkPTY = true, rows = 30, cols = 80, title = 'tty') {
+    const opts = {
+      runCommand: atom.config.get('term3.autoRunCommand'),
+      shellOverride: atom.config.get('term3.shellOverride'),
+      shellArguments: atom.config.get('term3.shellArguments'),
+      titleTemplate: atom.config.get('term3.titleTemplate'),
+      cursorBlink: atom.config.get('term3.cursorBlink'),
+      fontFamily: atom.config.get('term3.fontFamily'),
+      fontSize: atom.config.get('term3.fontSize'),
+      colors: getColors(),
+      forkPTY,
+      rows,
+      cols,
+    };
+
+    if (opts.shellOverride) {
+      opts.shell = opts.shellOverride;
+    } else {
+      opts.shell = process.env.SHELL || 'bash';
+    }
+
+    // opts.shellArguments or= ''
+    const editorPath = keypather.get(
+      atom,
+      'workspace.getEditorViews[0].getEditor().getPath()',
+    );
+    opts.cwd =
+      opts.cwd || atom.project.getPaths()[0] || editorPath || process.env.HOME;
+
+    const termView = new TermView(opts);
+    const model = store.addTerminal({
+      local: !!forkPTY,
+      term: termView,
+      title,
+    }).terminal;
+    const { id } = model;
+    termView.id = id;
+
+    termView.on('remove', this.handleRemoveTerm.bind(this));
+    termView.on('click', () => {
+      // get focus in the terminal
+      // avoid double click to get focus
+      return termView.term.focus();
+    });
+
+    termView.onDidChangeTitle(function() {
+      let newTitle = null;
+      if (forkPTY) {
+        newTitle = termView.getTitle();
+      } else {
+        newTitle = title + '-' + termView.getTitle();
+      }
+      return store.updateTerminal({ id: termView.id, title: newTitle });
+    });
+
+    if (typeof this.termViews.push === 'function') {
+      this.termViews.push(termView);
+    }
+    process.nextTick(() => this.emitter.emit('term', termView));
+    return termView;
+  },
+
+  splitTerm(direction) {
+    let pane;
+    const openPanesInSameSplit = atom.config.get('term3.openPanesInSameSplit');
+    const termView = this.createTermView();
+    direction = capitalize(direction);
+
+    const splitter = () => {
+      pane = activePane[`split${direction}`]({ items: [termView] });
+      activePane.termSplits[direction] = pane;
+      this.focusedTerminal = [pane, pane.items[0]];
+      return this.disposables.add(
+        this.attachSubscriptions(termView, pane.items[0], pane),
+      );
+    };
+
+    var activePane = atom.workspace.getActivePane();
+    if (!activePane.termSplits) {
+      activePane.termSplits = {};
+    }
+    if (openPanesInSameSplit) {
+      if (
+        activePane.termSplits[direction] &&
+        activePane.termSplits[direction].items.length > 0
+      ) {
+        pane = activePane.termSplits[direction];
+        const item = pane.addItem(termView);
+        pane.activateItem(item);
+        this.focusedTerminal = [pane, item];
+        return this.disposables.add(
+          this.attachSubscriptions(termView, item, pane),
+        );
+      } else {
+        return splitter();
+      }
+    } else {
+      return splitter();
+    }
+  },
+
+  pipeTerm(action) {
+    const editor = atom.workspace.getActiveTextEditor();
+    if (!editor) {
+      return;
+    }
+    const stream = (() => {
+      switch (action) {
+        case 'path':
+          return editor.getBuffer().file.path;
+        case 'selection':
+          return editor.getSelectedText();
+      }
+    })();
+
+    if (stream && this.focusedTerminal) {
+      let item;
+      if (Array.isArray(this.focusedTerminal)) {
+        let pane;
+        [pane, item] = Array.from(this.focusedTerminal);
+        pane.activateItem(item);
+      } else {
+        item = this.focusedTerminal;
+      }
+
+      item.input(stream.trim());
+      return item.focus();
+    }
+  },
+
+  handleRemoveTerm(termView) {
+    return this.termViews.splice(this.termViews.indexOf(termView), 1);
+  },
+
+  deactivate() {
+    this.termViews.forEach(view => view.exit());
+    this.termViews = [];
+    return this.disposables.dispose;
+  },
+
+  serialize() {
+    const termViewsState = this.termViews.map(view => view.serialize());
+    return { termViews: termViewsState };
+  },
+};
--- templates/list-view.js
+++ templates/list-view.js
@@ -1,26 +1,26 @@
 /** @jsx React.DOM */
 /* global HTMLElement */
-"use strict";
+'use strict';
 
-var React = require("react-atom-fork");
-var store = require("../store");
+var React = require('react-atom-fork');
+var store = require('../store');
 
 var TerminalView = React.createClass({
   propTypes: {
     terminal: React.PropTypes.object.isRequired,
   },
-  onMouseDown: function () {
+  onMouseDown: function() {
     this.props.terminal.open();
   },
-  render: function () {
+  render: function() {
     const t = this.props.terminal;
     return (
       <li onMouseDown={this.onMouseDown.bind(this, t.id)}>
-        <i className="icon icon-terminal"></i>
+        <i className="icon icon-terminal" />
         tty-{t.title}
       </li>
     );
-  }
+  },
 });
 
 function mapStateToProps(state) {
@@ -33,43 +33,43 @@ var ListView = React.createClass({
   propTypes: {
     terminals: React.PropTypes.array.isRequired,
   },
-  getInitialState: function () {
-    return ({ terminals: store.getState().terminals });
+  getInitialState: function() {
+    return { terminals: store.getState().terminals };
   },
-  componentWillMount: function () {
+  componentWillMount: function() {
     var that = this;
-    this.unsubscribeStore = store.subscribe(function () {
+    this.unsubscribeStore = store.subscribe(function() {
       that.setState({
         terminals: store.getState().terminals,
-      })
+      });
     });
   },
-  componentWillUnmount: function () {
+  componentWillUnmount: function() {
     if (typeof this.unsubscribeStore === 'function') {
       this.unsubscribeStore();
     }
   },
-  render: function () {
+  render: function() {
     // XXXX: Horrible hack to work around a bug in Atom. Sometimes, Atom will erase NODE_ENV when run from the command line
     if (!process.env.NODE_ENV) {
-      process.env.NODE_ENV = "production";
+      process.env.NODE_ENV = 'production';
     }
     const terminals = this.state.terminals;
     if (terminals == null || !terminals.length) {
-      return (<div></div>);
+      return <div />;
     }
-    const terms = terminals.map(function (t) {
-      return (<TerminalView terminal={t} key={t.id} />);
+    const terms = terminals.map(function(t) {
+      return <TerminalView terminal={t} key={t.id} />;
     });
     return (
       <div className="header">
-        <span className=""><i className="icon icon-terminal"></i>terminals</span>
+        <span className=""><i className="icon icon-terminal" />terminals</span>
         <ul>
           {terms}
         </ul>
       </div>
     );
-  }
+  },
 });
 
 const HTMLElementProto = Object.create(HTMLElement.prototype);
@@ -78,9 +78,8 @@ const HTMLElementProto = Object.create(HTMLElement.prototype);
 //   return;
 // };
 
-HTMLElementProto.attachedCallback = function () {
-  this.reactNode =
-    React.renderComponent(ListView({ store: store }), this);
+HTMLElementProto.attachedCallback = function() {
+  this.reactNode = React.renderComponent(ListView({ store: store }), this);
 };
 
 // HTMLElementProto.attributeChangedCallback = function (attrName, oldVal, newVal) {
@@ -91,4 +90,6 @@ HTMLElementProto.attachedCallback = function () {
 //   return;
 // };
 
-module.exports = document.registerElement('terminal-list-view', {prototype: HTMLElementProto});
+module.exports = document.registerElement('terminal-list-view', {
+  prototype: HTMLElementProto,
+});
