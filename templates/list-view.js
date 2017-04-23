f76e7bb672a36ec2aed03d7f8e45eb02918c6e20
Dan Brown <dan@stompydan.net>
2017-04-23 14:40:33 +0100

    Tidied up terminal tree view

--- templates/list-view.js
+++ templates/list-view.js
@@ -8,16 +8,23 @@ var store = require('../store');
 var TerminalView = React.createClass({
   propTypes: {
     terminal: React.PropTypes.object.isRequired,
+    selected: React.PropTypes.bool,
   },
-  onMouseDown: function() {
+
+  onMouseDown: function(e) {
+    e.stopPropagation();
     this.props.terminal.open();
   },
+
   render: function() {
     const t = this.props.terminal;
+    const sel = this.props.selected;
     return (
-      <li onMouseDown={this.onMouseDown.bind(this, t.id)}>
-        <i className="icon icon-terminal" />
-        tty-{t.title}
+      <li
+        onMouseDown={this.onMouseDown.bind(this)}
+        className={(sel ? 'selected' : '') + ' list-item'}
+      >
+        <span className="icon icon-terminal">{t.title}</span>
       </li>
     );
   },
@@ -27,41 +34,67 @@ var ListView = React.createClass({
   propTypes: {
     terminals: React.PropTypes.array.isRequired,
   },
+
   getInitialState: function() {
     return { terminals: store.getState().terminals };
   },
+
   componentWillMount: function() {
     var that = this;
     this.unsubscribeStore = store.subscribe(function() {
+      const { terminals, selectedTerminalId } = store.getState();
       that.setState({
-        terminals: store.getState().terminals,
+        terminals,
+        selectedTerminalId,
       });
     });
   },
+
   componentWillUnmount: function() {
     if (typeof this.unsubscribeStore === 'function') {
       this.unsubscribeStore();
     }
   },
+
+  toggleCollapse: function() {
+    const { collapsed } = this.state;
+    this.setState({ collapsed: !collapsed });
+  },
+
   render: function() {
     // XXXX: Horrible hack to work around a bug in Atom. Sometimes, Atom will erase NODE_ENV when run from the command line
     if (!process.env.NODE_ENV) {
       process.env.NODE_ENV = 'production';
     }
-    const terminals = this.state.terminals;
+    const { terminals, selectedTerminalId, collapsed } = this.state;
     if (terminals == null || !terminals.length) {
       return <div />;
     }
     const terms = terminals.map(function(t) {
-      return <TerminalView terminal={t} key={t.id} />;
+      return (
+        <TerminalView
+          terminal={t}
+          selected={t.id === selectedTerminalId}
+          key={t.id}
+        />
+      );
     });
     return (
-      <div className="header">
-        <span className=""><i className="icon icon-terminal" />terminals</span>
-        <ul>
-          {terms}
-        </ul>
-      </div>
+      <ol className="list-tree has-collapsable-children">
+        <li
+          className={`list-nested-item project-root ${collapsed ? 'collapsed' : 'expanded'}`}
+        >
+          <div
+            className="header list-item project-root-header"
+            onClick={this.toggleCollapse.bind(this)}
+          >
+            <span className="name icon icon-terminal">terminals</span>
+          </div>
+          <ol className="entries list-tree">
+            {terms}
+          </ol>
+        </li>
+      </ol>
     );
   },
 });
